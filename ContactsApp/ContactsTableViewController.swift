3f438004d1da71ab8ce05856681225c3668aa56c
julienKeyvalue <julien@keyvalue.systems>
2021-08-30 16:17:50 +0530

    Code reduced

--- ContactsApp/ContactsTableViewController.swift
+++ ContactsApp/ContactsTableViewController.swift
@@ -46,35 +46,35 @@ class ContactsTableViewController: UIViewController, UITableViewDelegate, UITabl
         return 75
     }
 
-    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    func isPhoneNoValid(value: String) -> Bool {
+        let PHONE_REGEX = "^\\d{10}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+        let result = phoneTest.evaluate(with: value)
+        return result
+    }
+    func isValidName(Input:String) -> Bool {
+        let RegEx = "^\\w{6,18}$"
+        let Test = NSPredicate(format:"SELF MATCHES %@", RegEx)
+        return Test.evaluate(with: Input)
+    }
     @IBAction func addButtonPressed(_ sender: UIButton) {
         var nameField = UITextField()
         var numberField = UITextField()
         var emailField = UITextField()
         nameField.delegate=self
         
-        func isValidEmail(_ email: String) -> Bool {
-            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-            
-            let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-            return emailPred.evaluate(with: email)
-        }
-        func isPhoneNoValid(value: String) -> Bool {
-            let PHONE_REGEX = "^\\d{10}$"
-            let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
-            let result = phoneTest.evaluate(with: value)
-            return result
-        }
-        func isValidName(Input:String) -> Bool {
-            let RegEx = "^\\w{6,18}$"
-            let Test = NSPredicate(format:"SELF MATCHES %@", RegEx)
-            return Test.evaluate(with: Input)
-        }
+        
         
         let alert=UIAlertController( title: "Add new Contact", message: " ", preferredStyle: .alert)
         let action=UIAlertAction(title: "Add", style: .default) { (action) in
             
-            if(!isValidName(Input: nameField.text!)){
+            if(!self.isValidName(Input: nameField.text!)){
                 let nameErrorAlert=UIAlertController(title: "Username is not Valid", message: "", preferredStyle: .alert)
                 let back=UIAlertAction(title: "Back", style: .default) { action in
                     self.dismiss(animated: true) {
@@ -85,7 +85,7 @@ class ContactsTableViewController: UIViewController, UITableViewDelegate, UITabl
                 self.present(nameErrorAlert, animated: true, completion: nil)
             }
             
-            if(!isPhoneNoValid(value:numberField.text!)){
+            if(!self.isPhoneNoValid(value:numberField.text!)){
                 let phoneNumberErrorAlert=UIAlertController(title: "Phone Number is not valid ", message: "", preferredStyle: .alert)
                 let back=UIAlertAction(title: "Back", style: .default) { action in
                     self.dismiss(animated: true) {
@@ -95,7 +95,7 @@ class ContactsTableViewController: UIViewController, UITableViewDelegate, UITabl
                 phoneNumberErrorAlert.addAction(back)
                 self.present(phoneNumberErrorAlert, animated: true, completion: nil)
             }
-            if(!isValidEmail(emailField.text!)){
+            if(!self.isValidEmail(emailField.text!)){
                 let emailErrorAlert=UIAlertController(title: "Not a valid Email", message: "", preferredStyle: .alert)
                 let back=UIAlertAction(title: "Back", style: .default) { action in
                     self.dismiss(animated: true) {
@@ -106,7 +106,7 @@ class ContactsTableViewController: UIViewController, UITableViewDelegate, UITabl
                 self.present(emailErrorAlert, animated: true, completion: nil)
                 
             }
-            if(isValidName(Input: nameField.text!) && isValidEmail(emailField.text!) && isPhoneNoValid(value: numberField.text!)){
+            else{
                 self.model.contactList.append([nameField.text!,numberField.text!,emailField.text!])
                 self.contactTable.reloadData()
             }
